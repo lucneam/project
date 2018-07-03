aeb9cfbc6a602a9201ee15a8a7377e5e64904e7e
root <x3nguyenluc@gmail.com>
2018-07-03 15:43:23 +0700

    Initialize repository

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # create rousources in subdomain api
+  namespace :api, path: '/', constraints: { subdomain: 'api' } do
+    resources :users
+  end
+
 end

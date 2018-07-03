17e8eb57e0ba6ecc7544336c922c9b8bdb306bbd
root <x3nguyenluc@gmail.com>
2018-07-03 15:46:38 +0700

    Add a Static Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-
+  get 'static_pages/home'
+  get 'static_pages/help'
+	 root 'application#hello'
+	 
   # create rousources in subdomain api
   namespace :api, path: '/', constraints: { subdomain: 'api' } do
     resources :users
+
   end
 
 end
